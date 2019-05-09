e76a6a93b5d4ed248c1c50f9b645be40605f90ea
Ahmed Elkhateeb <ahmedelkhateeb35777@gmail.com>
2019-05-09 11:18:40 +0200

    Adding Error messages

--- dist/es2015-polyfills.js
+++ dist/es2015-polyfills.js
@@ -7006,8 +7006,8 @@ for (var collections = getKeys(DOMIterables), i = 0; i < collections.length; i++
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/ahmed/Desktop/Angular-Task/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-polyfills.js");
-module.exports = __webpack_require__(/*! /home/ahmed/Desktop/Angular-Task/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-jit-polyfills.js");
+__webpack_require__(/*! /home/ahmed/Desktop/To-Do-Task/To-Do-Client/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-polyfills.js");
+module.exports = __webpack_require__(/*! /home/ahmed/Desktop/To-Do-Task/To-Do-Client/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-jit-polyfills.js");
 
 
 /***/ })
--- dist/styles.js
+++ dist/styles.js
@@ -598,8 +598,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/ahmed/Desktop/Angular-Task/node_modules/@angular/material/prebuilt-themes/indigo-pink.css */"./node_modules/@angular/material/prebuilt-themes/indigo-pink.css");
-module.exports = __webpack_require__(/*! /home/ahmed/Desktop/Angular-Task/src/styles.css */"./src/styles.css");
+__webpack_require__(/*! /home/ahmed/Desktop/To-Do-Task/To-Do-Client/node_modules/@angular/material/prebuilt-themes/indigo-pink.css */"./node_modules/@angular/material/prebuilt-themes/indigo-pink.css");
+module.exports = __webpack_require__(/*! /home/ahmed/Desktop/To-Do-Task/To-Do-Client/src/styles.css */"./src/styles.css");
 
 
 /***/ })
